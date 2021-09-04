54717c5a09eb7d5c70d58974fe83e5a6bd2372a4
Pavel <pl.hancharou@gmail.com>
2021-09-04 13:59:52 +0300

    fix: add default styles

--- develop/src/index.tsx
+++ develop/src/index.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App/index';
 
+import './index.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
