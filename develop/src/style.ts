52dc78b17824a082508b056505bfab1d1cd0c94e
Pavel <pl.hancharou@gmail.com>
2021-09-07 13:00:24 +0300

    change: change extension file / add default styles

---
+++ develop/src/style.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 30px;
+    color: #000000;
+    background-color: #ffffff;
+  }
+
+  ul {
+    list-style: none
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  a, button, input {
+    font-size: inherit;
+    line-height: inherit;
+  }
+
+  a, button {
+    cursor: pointer;
+  }
+
+  input {
+    font-family: 'Roboto', sans-serif;
+    text-overflow: ellipsis;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
