b4468e8a87d3dd9182db5155918cb8f3975e60ec
Pavel <pl.hancharou@gmail.com>
2021-09-07 13:08:01 +0300

    feat: create Main component

---
+++ develop/src/component/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Unauthorized from '../pages/Unauthorized';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <Unauthorized />
+    </main>
+  );
+};
+
+export default Main;
