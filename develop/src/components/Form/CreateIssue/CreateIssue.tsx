fe1ec16a521cf607d32fed2684a44b9c268c5404
Pavel <pl.hancharou@gmail.com>
2021-09-10 18:37:33 +0300

    fix: add content in CreateIssueForm

--- develop/src/components/Form/CreateIssue/CreateIssue.tsx
+++ develop/src/components/Form/CreateIssue/CreateIssue.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
-import { FormTitle } from '../../../shared/Form';
+import { FlexBox } from '../../../shared/FlexBox';
+import { FormTitle, Input, Label } from '../../../shared/Form';
 
 export const CreateIssueForm: FC = (): JSX.Element => {
   const { register, handleSubmit } = useForm();
@@ -8,7 +9,28 @@ export const CreateIssueForm: FC = (): JSX.Element => {
   return (
     <>
       <FormTitle marginBottom="75px">Create Issue</FormTitle>
-      <form />
+      <form>
+        <FlexBox flexDirection="column" width="569px">
+          <FlexBox justifyContent="space-between" alignItems="center" margin="0 0 47px 0">
+            <Label htmlFor="title">Title:</Label>
+            <Input id="title" height="47px" width="420px" />
+          </FlexBox>
+          <FlexBox justifyContent="space-between" alignItems="center" margin="0 0 47px 0">
+            <Label htmlFor="link">Link:</Label>
+            <Input id="link" height="47px" width="420px" />
+          </FlexBox>
+          <FlexBox alignItems="center" margin="0 0 47px 0">
+            <Label htmlFor="priority" margin="0 95px 0 0">
+              Priority:
+            </Label>
+            <select id="priority">
+              <option value="female">Low</option>
+              <option value="male">Middle</option>
+              <option value="other">Hight</option>
+            </select>
+          </FlexBox>
+        </FlexBox>
+      </form>
     </>
   );
 };
