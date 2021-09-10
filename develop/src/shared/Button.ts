2d27eec160bc6b8ed97f97e5491dc3bb80ecfb4a
Pavel <pl.hancharou@gmail.com>
2021-09-10 11:57:53 +0300

    fix: add ButtonBox styled-component

--- develop/src/shared/Button.ts
+++ develop/src/shared/Button.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 type ButtonType = {
   padding?: string;
   color?: string;
-  backgroundColor?: string;
+  background?: string;
   borderRadius?: string;
 };
 
 export const Button = styled.button<ButtonType>`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -15,7 +16,7 @@ export const Button = styled.button<ButtonType>`
   font-family: 'Ruda', sans-serif;
   font-weight: 700;
   color: ${({ color }) => color};
-  background-color: ${({ backgroundColor }) => backgroundColor};
+  background: ${({ background }) => background};
   border: 1px solid #496a81;
   border-radius: ${({ borderRadius }) => borderRadius};
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -24,6 +25,21 @@ export const Button = styled.button<ButtonType>`
 Button.defaultProps = {
   padding: '7px 30px',
   color: '#ffffff',
-  backgroundColor: '#2b3a67',
+  background: '#2b3a67',
   borderRadius: '3px'
+};
+
+type ButtonBoxType = {
+  margin?: string;
+};
+
+export const ButtonBox = styled.div<ButtonBoxType>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: ${({ margin }) => margin};
+`;
+
+ButtonBox.defaultProps = {
+  margin: '0'
 };
