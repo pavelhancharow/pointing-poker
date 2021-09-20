3696166db7c6c55e066ef155f1a1b7cdf10db265
Nevold <nevold@tut.by>
2021-09-20 23:42:23 +0300

    feat: add axios client

--- develop/src/store/action-creaters/action-creaters.ts
+++ develop/src/store/action-creaters/action-creaters.ts
@@ -1,8 +1,16 @@
 import { Dispatch } from '@reduxjs/toolkit';
+import { axiosInstance } from '../../services/api';
 import { userAction } from '../reducer/userReducer';
 
 export const getUserInfo = async (dispatch: Dispatch) => {
-  const rensponce = await fetch('https://jsonplaceholder.typicode.com/users/');
-  const data = await rensponce.json();
-  return dispatch(userAction(data));
+  // use fetch
+  // const rensponce = await fetch('https://jsonplaceholder.typicode.com/users/');
+  // const data = await rensponce.json();
+  //  return dispatch(userAction(data));
+
+  // or
+
+  // use axios
+  const rensponce = await axiosInstance.get('users');
+  return dispatch(userAction(rensponce.data));
 };
