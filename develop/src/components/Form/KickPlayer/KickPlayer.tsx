0de9b0047b4f3bf921fa7f21c66ad9801dd9c07a
eugenemp <phoenixlp91@mail.ru>
2021-09-22 09:48:33 +0300

    refactor: add possibility to change KickPlayer modal mode, delete unneeded Kick component

--- develop/src/components/Form/KickPlayer/KickPlayer.tsx
+++ develop/src/components/Form/KickPlayer/KickPlayer.tsx
@@ -4,13 +4,23 @@ import { FormTitle } from '../../../shared/style/FormTitle';
 import { Button } from '../../../shared/style/Button';
 import { Text } from './style';
 
-export const KickPlayer: FC = (): JSX.Element => {
+type KickPlayerProps = {
+  type: 'master' | 'player';
+};
+
+export const KickPlayer: FC<KickPlayerProps> = ({ type }): JSX.Element => {
   return (
     <>
       <FormTitle marginBottom="75px">Kick Player</FormTitle>
-      <Text>
-        Are you really want to remove player <span>David Blane</span> from game session?
-      </Text>
+      {type === 'master' ? (
+        <Text>
+          Are you really want to remove player <span>David Blane</span> from game session?
+        </Text>
+      ) : (
+        <Text>
+          <span>Dayana Ross</span> want to kick member <span>David Blane</span>. Do you agree with it?
+        </Text>
+      )}
       <FlexBox display="flex" alignItems="center" justifyContent="space-between" width="100%" margin="97px 0 0 0">
         <Button primary>Yes</Button>
         <Button>No</Button>
